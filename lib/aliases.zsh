8887034ba1e3c580bb7e75ed36a4ea457db8e64e
Ian White <ian.white@t-3.com>
2014-07-14 12:34:24 -0500

    Update aliases.zsh

---
+++ lib/aliases.zsh
@@ -0,0 +1,58 @@
+# Push and pop directories on directory stack
+alias pu='pushd'
+alias po='popd'
+
+# Basic directory operations
+alias ...='cd ../..'
+alias -- -='cd -'
+
+# Super user
+alias _='sudo'
+alias please='sudo'
+
+#alias g='grep -in'
+
+# Show history
+if [ "$HIST_STAMPS" = "mm/dd/yyyy" ]
+then
+    alias history='fc -fl 1'
+elif [ "$HIST_STAMPS" = "dd.mm.yyyy" ]
+then
+    alias history='fc -El 1'
+elif [ "$HIST_STAMPS" = "yyyy-mm-dd" ]
+then
+    alias history='fc -il 1'
+else
+    alias history='fc -l 1'
+fi
+# List direcory contents
+alias lsa='ls -lah'
+alias l='ls -lah'
+alias ll='ls -lh'
+alias la='ls -lAh'
+
+alias afind='ack-grep -il'
+
+# Git commit add all with msg
+alias gcmsga='git commit -am'
+# Git add -A plus git commit -m
+alias gaacmsg='git add -A && git commit -m'
+# Git push origin master
+alias gpom='git push origin master'
+
+# Grunt aliases
+alias grban='grunt build:android'
+alias grran='grunt run:android'
+alias grean='grunt emulate:android'
+
+alias grbios='grunt build:ios'
+alias grrios='grunt run:ios'
+alias greios='grunt emulate:ios'
+
+# Rails aliases
+alias bexrsp='bundle exec rspec'
+
+
+
+
+
