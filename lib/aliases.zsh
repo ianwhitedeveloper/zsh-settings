018d85ebdee14a5d0fa48d70090c8e5574426047
Ian White <ian.white@t-3.com>
2016-05-02 13:31:01 -0500

    add some aliases yo

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -104,6 +104,10 @@ alias astash='git stash apply'
 alias scramNodeModules='find . -name "node_modules" -exec rm -rf "{}" +'
 
 alias clean='git clean -f'
+# Return IP address as a string
+alias myIpAddy='dig TXT +short o-o.myaddr.l.google.com @ns1.google.com'
+
+alias gzipEmAll="find . -type f ! -name '*.gz' -exec gzip "{}" \;"
 
 # Recursively remove all node_modules folders to clear up space
 alias scramNodeModules='find . -name "node_modules" -exec rm -rf "{}" +'
